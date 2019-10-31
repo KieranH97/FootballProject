ec5c92a33a56d8fd258ccf83a9205bd22a9d7fd6
Kieran <Kieranhale@live.co.uk>
2019-10-31 11:15:27 +0000

    FootballAppLatestVersion

--- src/main/java/com/example/Controller/DBPController.java
+++ src/main/java/com/example/Controller/DBPController.java
@@ -1,11 +1,16 @@
 package com.example.Controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Entity.Players;
@@ -13,33 +18,47 @@ import com.example.Repository.PlayersRepo;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/players")
 public class DBPController {
 	
 	@Autowired
 	private PlayersRepo repo;
 	
-	@GetMapping("/Showall")
-	public ArrayList <Players> Showall() {
+	@GetMapping
+	public List<Players> Showall() {
 		return repo.findAll();
 	}
-	@GetMapping("/findByClub/{N}")
-	public ArrayList <Players> findByClub(@PathVariable String N) {
-		return repo.findByClub(N);
+
+	@PostMapping
+	public Players createplayer(@RequestBody Players player) {
+		return repo.saveAndFlush(player);
 	}
-	@GetMapping("/findByName/{N}")
-	public ArrayList <Players> findByName(@PathVariable String N) {
-		return repo.findByName(N);
+
+	@DeleteMapping("/{N}")
+	public String deleteByName(@PathVariable String N) {
+		repo.deleteById(N);
+		return "Player Deleted";
 	}
-	@GetMapping("/findByAge/{N}")
-	public ArrayList <Players> findByAge(@PathVariable int N) {
-		return repo.findByAge(N);
-	}
-	@GetMapping("/findByPosition/{N}")
-	public ArrayList <Players> findByPosition(@PathVariable String N) {
-		return repo.findByPosition(N);
-	}
+
 }
+
 	
+//	@GetMapping("/findByClub/{N}")
+//	public ArrayList <Players> findByClub(@PathVariable String N) {
+//		return repo.findByClub(N);
+//	}
+//	@GetMapping("/findByName/{N}")
+//	public Players findByName(@PathVariable String N) {
+//		return repo.getOne(N);
+//	}
+//	@GetMapping("/findByAge/{N}")
+//	public ArrayList <Players> findByAge(@PathVariable int N) {
+//		return repo.findByAge(N);
+//	}
+//	@GetMapping("/findByPosition/{N}")
+//	public ArrayList <Players> findByPosition(@PathVariable String N) {
+//		return repo.findByPosition(N);
+//	}
 	
 //	@GetMapping("/filterdept1/{department}/{subject}")
 //	public ArrayList <Players> filteringdepartments(@PathVariable String department, @PathVariable String subject) {
